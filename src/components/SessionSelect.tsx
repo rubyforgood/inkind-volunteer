b883f0e7dfde2f706b81f6190f3d9f89a34e1202
sethgabrielbrady <sethgabrielbrady@gmail.com>
2021-09-25 14:42:38 -0400

    Adds no survey sessions notification

--- src/components/SessionSelect.tsx
+++ src/components/SessionSelect.tsx
@@ -11,17 +11,27 @@ export const SessionSelect = ({
     <div>
       <p className="text-lg font-semibold py-2 text-left">Recent Sessions</p>
 
+      {sessions.length === 0 &&
+        <div className="flex content-around mt-4">
+          <p className="text-lg font-semibold py-2 text-center rounded-full bg-red-200 w-64 h-64 pt-24  mx-auto">
+            No Sessions
+          </p>
+        </div>
+      }
+
       {sessions?.map((session: Session) => {
+        console.log(sessions.length)
         const name = `${session.student}`;
         const date = `${session.date}`;
 
         return (
           <>
-            <div className="w-full inline-block text-left my-2 bg-green-50">
-              <p className="block">{name}</p>
-              <p className="block">
-                {date}
-              </p>
+            <div className="w-full text-left my-2 bg-blue-50 p-2">
+              <img src="" className="text-center rounded-full bg-red-200 w-10 h-10 inline-block mr-3 inline-block" />
+              <div className="inline-block">
+                <p className="block">{name}</p>
+                <p className="block text-xs">{date}</p>
+              </div>
             </div>
           </>
         );
--- src/components/StudentSelect.tsx
+++ src/components/StudentSelect.tsx
@@ -17,12 +17,15 @@ export const StudentSelect = ({
 
         return (
           <>
-            <div className="w-full inline-block text-left my-2 bg-green-50">
-              <input type="radio" value={student.id} key={student.id} className="mx-4 hidden" onChange={onChange} name="studentId" />
-              <label className="inline-block">
-                {fullName}
-              </label>
-              <p>{age}</p>
+            <div className="w-full inline-block text-left my-2 bg-green-50 p-2">
+              <div className="inline-block ">
+                <input type="radio" value={student.id} key={student.id} className="mx-4 hidden" onChange={onChange} name="studentId" />
+                <label className="block">
+                  <img src="" className="text-center rounded-full bg-red-200 w-10 h-10 inline-block mr-3" />
+                  {fullName}
+                  <p className="text-xs block pl-12"> Age {age}</p>
+                </label>
+              </div>
             </div>
           </>
         );
