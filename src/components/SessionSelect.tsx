26a01188f34a2c60337c04104cf058b03dc1c197
sethgabrielbrady <sethgabrielbrady@gmail.com>
2021-09-27 17:11:23 -0400

    Cleanups the students and volunteers.

--- src/components/SessionSelect.tsx
+++ src/components/SessionSelect.tsx
@@ -27,9 +27,9 @@ export const SessionSelect = ({
         return (
           <>
             <div className="px-2">
-              <div className="w-full inline-block text-left my-2 shadow border rounded py-2 px-3 text-gray-700 leading-tight border-blue-300 bg-blue-50">
+              <div className="w-full flex flex-row inline-block text-left my-2 shadow border rounded py-2 px-3 text-gray-700 leading-tight border-blue-300 bg-blue-50">
                 <img src="" className="text-center rounded-full bg-red-200 w-10 h-10 inline-block mr-3 inline-block" />
-                <div className="inline-block">
+                <div className="inline-block flex flex-col">
                   <p className="block">{name}</p>
                   <p className="block text-xs">{date}</p>
                 </div>
--- src/components/StudentSelect.tsx
+++ src/components/StudentSelect.tsx
@@ -15,14 +15,11 @@ export const StudentSelect = ({
         return (
           <>
             <div className="px-2">
-              <div className="w-full inline-block text-left my-2 shadow border rounded py-2 px-3 text-gray-700 leading-tight border-green-300 bg-green-50">
-                <div className="inline-block ">
-                  <input type="radio" value={student.id} key={student.id} className="mx-4 hidden" onChange={onChange} name="studentId" />
-                  <label className="block">
-                    <img src="" className="text-center rounded-full bg-red-200 w-10 h-10 inline-block mr-3" />
-                    {student.name}
-                    <p className="text-xs block pl-12">{student.dateOfBirth}</p>
-                  </label>
+              <div className="flex flex-row w-full inline-block text-left my-2 shadow border rounded py-2 px-3 text-gray-700 leading-tight border-green-300 bg-green-50">
+                <img src="" className="text-center rounded-full bg-red-200 w-10 h-10 inline-block mr-3 inline-block" />
+                <div className="inline-block flex flex-col ">
+                  <p className="block">{student.name}</p>
+                  <p className="block text-xs">{student.dateOfBirth}</p>
                 </div>
               </div>
             </div>
