e2d8d6a9a454573502d365e4f2cc3be84e797db7
Garett Arrowood <garettarrowood@gmail.com>
2021-10-10 17:19:15 -0400

    Global font only needs to be in body tag

--- src/components/StudentSection.tsx
+++ src/components/StudentSection.tsx
@@ -16,8 +16,8 @@ export const StudentSection = ({
               <div className="flex flex-row w-full inline-block text-left my-2 shadow border rounded py-2 px-3 text-gray-700 leading-tight border-green-300 bg-green-50">
                 <img src="" className="text-center rounded-full bg-red-200 w-10 h-10 inline-block mr-3 inline-block" />
                 <div className="inline-block flex flex-col ">
-                  <p className="block font-nunito">{student.name}</p>
-                  <p className="block text-xs font-nunito">{student.dateOfBirth}</p>
+                  <p className="block">{student.name}</p>
+                  <p className="block text-xs">{student.dateOfBirth}</p>
                 </div>
               </div>
             </div>
