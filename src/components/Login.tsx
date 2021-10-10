e112cbb60688e49733a0c23a363066196b8bfd9b
Garett Arrowood <garettarrowood@gmail.com>
2021-10-10 16:24:35 -0400

    Add & use Nunito font throughout app

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -41,14 +41,14 @@ export const Login = ({ setUser }: LoginProps): JSX.Element => {
       <div className="w-full max-w-md bg-gray-800">
         <form className=" bg-white shadow-md rounded px-8 py-8 pt-8">
           <div className="px-4 pb-4">
-            <label htmlFor="email" className="text-sm block font-bold  pb-2">
+            <label htmlFor="email" className="text-sm font-nunito block font-bold  pb-2">
               EMAIL ADDRESS
             </label>
             <input
               type="email"
               name="email"
               id="email"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300 "
+              className="shadow font-nunito appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300 "
               placeholder="Johnbull@example.com"
               onBlur={(event: React.FormEvent) =>
                 setUserEmail((event.currentTarget as HTMLInputElement).value)
@@ -56,14 +56,14 @@ export const Login = ({ setUser }: LoginProps): JSX.Element => {
             />
           </div>
           <div className="px-4 pb-4">
-            <label htmlFor="password" className="text-sm block font-bold pb-2">
+            <label htmlFor="password" className="text-sm font-nunito block font-bold pb-2">
               PASSWORD
             </label>
             <input
               type="password"
               name="password"
               id="password"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300"
+              className="shadow font-nunito appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300"
               placeholder="Enter your password"
               onBlur={(event: React.FormEvent) =>
                 setPassword((event.currentTarget as HTMLInputElement).value)
@@ -72,7 +72,7 @@ export const Login = ({ setUser }: LoginProps): JSX.Element => {
           </div>
           <div>
             <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              className="bg-blue-500 font-nunito hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               type="button"
               onClick={() => login()}
             >
