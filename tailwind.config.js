b2cab68ab1b34fb539ebbee25ebe21b23507c619
Rimenes Ribeiro <rima@rimenesribeiro.com>
2021-10-12 13:58:06 -0300

    Fix Tailwind CSS colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,11 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false,
   theme: {
-    colors: {
-      purple: {
-        light: "#d1c4e9",
+    extend: {
+      colors: {
+        'purple-light': '#85d7ff'
       },
     },
-    extend: {},
     fontFamily: {
       'nunito': ['nunito', 'sans-serif']
     },
