5436028420351dcb92626e2b30399a53c6b2fb8b
Gabriel Pereira <gmfvpereira@gmail.com>
2021-10-13 10:45:10 -0300

    Add rounded border to student info

--- src/components/StudentShow.tsx
+++ src/components/StudentShow.tsx
@@ -30,33 +30,33 @@ export const StudentShow = (): JSX.Element | null => {
       <h1 className="text-xl py-2">{name}</h1>
       <p className="text-lg py-2 text-left">Student Information</p>
 
-      <div className="grid grid-cols-2 my-4 rounded-lg">
-        <div className="flex flex-col items-start bg-white p-1 border-b-2 p-3">
+      <div className="grid grid-cols-2 my-4 rounded-lg bg-white">
+        <div className="flex flex-col items-start p-1 border-b-2 p-3">
           <span className="text-xs">Guardian Name</span>
           <span className="text-base font-medium">{guardianName}</span>
         </div>
 
-        <div className="flex flex-col items-start bg-white p-1 border-b-2 p-3">
+        <div className="flex flex-col items-start p-1 border-b-2 p-3">
           <span className="text-xs">Guardian Phone</span>
           <span className="text-base font-medium">{guardianPhoneNumber}</span>
         </div>
 
-        <div className="flex flex-col items-start bg-white p-1 border-b-2 p-3">
+        <div className="flex flex-col items-start p-1 border-b-2 p-3">
           <span className="text-xs">Emergency Contact</span>
           <span className="text-base font-medium">{emergencyContactName}</span>
         </div>
 
-        <div className="flex flex-col items-start bg-white p-1 border-b-2 p-3">
+        <div className="flex flex-col items-start p-1 border-b-2 p-3">
           <span className="text-xs">Emergency Phone</span>
           <span className="text-base font-medium">{emergencyContactPhoneNumber}</span>
         </div>
 
-        <div className="flex flex-col items-start bg-white p-1 p-3">
+        <div className="flex flex-col items-start p-1 p-3">
           <span className="text-xs">Student's Age</span>
           <span className="text-base font-medium">{getAge(dateOfBirth)}</span>
         </div>
 
-        <div className="flex flex-col items-start bg-white p-1" />
+        <div className="flex flex-col items-start p-1" />
       </div>
 
       <button className="bg-purple text-neutral px-5 py-3 rounded-md">BEGIN SESSION SURVEY</button>
