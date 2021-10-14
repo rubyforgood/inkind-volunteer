9b1d3e3e8ac603a6f8a6232796ac03104844f1a9
Gabriel Pereira <gmfvpereira@gmail.com>
2021-10-14 11:17:03 -0300

    Add buttons and align options

--- src/components/SingleSelectionOption.tsx
+++ src/components/SingleSelectionOption.tsx
@@ -11,12 +11,17 @@ export const SingleSelectionOption = ({ options }: SingleSelectionOptionProps): 
     <section className="w-full px-4 py-8 pt-8 text-gray-dark h-screen">
       {options.map((option: GetSurvey_survey_questions_options) => {
         return (
-          <p>
-            <label>{option.label}</label>
+          <p className="text-left">
             <input type="radio" name="options" id={option.id} value={option.label} />
+            <label className="ml-2" htmlFor="options">{option.label}</label>
           </p>
         )}
       )}
+
+      <div className="grid grid-cols-2 gap-4 my-4">
+        <button className="bg-neutral-50 text-neutral-900 px-5 py-3 rounded">SKIP</button>
+        <button className="bg-primary-500 text-neutral-50 px-5 py-3 rounded">NEXT</button>
+      </div>
     </section>
   )
 }
