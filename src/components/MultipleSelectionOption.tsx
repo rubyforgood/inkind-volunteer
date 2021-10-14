f4e3ea5cd6d23141f7d352e1e6b59802e3424d00
Gabriel Pereira <gmfvpereira@gmail.com>
2021-10-14 11:32:41 -0300

    Add multiple select options

--- src/components/MultipleSelectionOption.tsx
+++ src/components/MultipleSelectionOption.tsx
@@ -1,18 +1,16 @@
-import React from "react"
-
 import { GetSurvey_survey_questions_options } from "graphql/queries/__generated__/GetSurvey"
 
-interface SingleSelectionOptionProps {
+interface MultipleSelectionOptionProps {
   options: GetSurvey_survey_questions_options[]
 }
 
-export const SingleSelectionOption = ({ options }: SingleSelectionOptionProps): JSX.Element | null => {
+export const MultipleSelectionOption = ({ options }: MultipleSelectionOptionProps): JSX.Element | null => {
   return (
     <section className="w-full text-gray-dark h-screen px-4 py-8">
       {options.map((option: GetSurvey_survey_questions_options) => {
         return (
           <p className="text-left" key={option.id}>
-            <input type="radio" name="options" id={option.id} value={option.label} />
+            <input type="checkbox" name="options" id={option.id} value={option.label} />
             <label className="ml-2" htmlFor="options">{option.label}</label>
           </p>
         )}
--- src/components/SurveyShow.tsx
+++ src/components/SurveyShow.tsx
@@ -3,10 +3,10 @@ import { useQuery } from "@apollo/client"
 import { useParams } from "react-router-dom"
 
 import { GetSurveyQuery } from "graphql/queries/GetSurvey"
-import { GetSurvey } from "graphql/queries/__generated__/GetSurvey"
+import { GetSurvey, GetSurvey_survey_questions } from "graphql/queries/__generated__/GetSurvey"
 
 import { SingleSelectionOption } from "./SingleSelectionOption"
-
+import { MultipleSelectionOption } from "./MultipleSelectionOption"
 interface SurveyShowProps {
   surveyId: string
 }
@@ -20,6 +20,7 @@ export const SurveyShow = (): JSX.Element | null => {
   if (loading || !data) return null
 
   const { questions } = data.survey
+  console.log(questions)
 
   if (!questions) return null
 
@@ -33,15 +34,22 @@ export const SurveyShow = (): JSX.Element | null => {
     setCurrentQuestionIndex(currentQuestionIndex + 1)
   }
 
-  console.log(data.survey)
+  const renderQuestion = (question: GetSurvey_survey_questions) => {
+    if (question.options) {
+      if (question.type == "SurveySingleSelectQuestion") {
+        return <SingleSelectionOption options={question.options} />
+      }
+      if (question.type == "SurveyMultiSelectQuestion") {
+        return <MultipleSelectionOption options={question.options} />
+      }
+    }
+  }
 
   return (
     <section className="w-full px-4 py-8 pt-8 text-gray-dark h-screen">
       <p>{currentQuestion.prompt}</p>
-      {currentQuestion.type == "SurveySingleSelectQuestion" &&
-        currentQuestion.options &&
-        (<SingleSelectionOption options={currentQuestion.options} />)
-      }
+
+      {renderQuestion(currentQuestion)}
 
       <div className="fixed bottom-20 inset-x-0 w-full grid grid-cols-2 gap-4 px-4 py-8">
         <button className="bg-neutral-50 text-neutral-900 px-5 py-3 rounded" onClick={onSkip}>SKIP</button>
