ebdabc71cf11c71248ba2346a21b18801123df58
Gillian Wenhold <gillian.wenhold@powerhrg.com>
2021-10-14 11:39:03 -0400

    Extract session item to separate component

---
+++ src/components/SessionItem.tsx
@@ -0,0 +1,29 @@
+import React from "react"
+
+import { SurveyResponse } from "models/SurveyResponse"
+import { Avatar } from "./Avatar"
+
+interface SessionItemProps {
+  response: SurveyResponse,
+}
+
+
+export const SessionItem = ({ response }: SessionItemProps): JSX.Element => {
+  const name = `${response.student.name}`
+  const date = new Date(response?.meetingDuration?.startedAt || "2021")
+  const formattedDate = `${date.toLocaleString('default', { month: 'long' })} ${date.getDate()}, ${date.getFullYear()}`
+
+  return (
+    <React.Fragment key={response.id}>
+      <div>
+        <div className="flex flex-row w-full inline-block text-left my-1 p-4 leading-tight bg-white rounded-md">
+          <Avatar initials={response.student.initials} />
+          <div className="inline-block flex flex-col">
+            <p className="block font-semibold">{name}</p>
+            <p className="block text-xs">{formattedDate}</p>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  )
+}
--- src/components/SessionSection.tsx
+++ src/components/SessionSection.tsx
@@ -1,11 +1,13 @@
-import { SurveyResponse } from "models/SurveyResponse"
-import { Avatar } from "./Avatar"
 import React from "react"
 import { useQuery } from "@apollo/client"
+
+import { SurveyResponse } from "models/SurveyResponse"
 import { GetSurveyResponsesQuery } from "graphql/queries/GetSurveyResponses"
-import { QueryError } from "./QueryError"
 import { GetSurveyResponses } from "graphql/queries/__generated__/GetSurveyResponses"
+
+import { QueryError } from "./QueryError"
 import NoSessionSVG from "./noSessions.svg"
+import { SessionItem } from "./SessionItem"
 
 export const SessionSection = (): JSX.Element => {
   const { data, loading, error } = useQuery<GetSurveyResponses>(GetSurveyResponsesQuery)
@@ -27,25 +29,12 @@ export const SessionSection = (): JSX.Element => {
         </div>
       }
 
-      {data?.surveyResponses?.map((response: SurveyResponse) => {
-        const name = `${response.student.name}`
-        const date = new Date(response?.meetingDuration?.startedAt || "2021")
-        const formattedDate = `${date.toLocaleString('default', { month: 'long' })} ${date.getDate()}, ${date.getFullYear()}`
-
-        return (
-          <React.Fragment key={response.id}>
-            <div>
-              <div className="flex flex-row w-full inline-block text-left my-1 p-4 leading-tight bg-white rounded-md">
-                <Avatar initials={response.student.initials} />
-                <div className="inline-block flex flex-col">
-                  <p className="block font-semibold">{name}</p>
-                  <p className="block text-xs">{formattedDate}</p>
-                </div>
-              </div>
-            </div>
-          </React.Fragment>
-        )
-      })}
+      {data?.surveyResponses?.map((response: SurveyResponse) => (
+        <SessionItem
+            key={response.id}
+            response={response}
+        />
+      ))}
     </div>
   )
 }
