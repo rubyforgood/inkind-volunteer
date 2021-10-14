e81768505babe553345b3556c98c4755d5916084
Gillian Wenhold <gillian.wenhold@powerhrg.com>
2021-10-14 14:07:34 -0400

    Remove react fragment

--- src/components/SessionItem.tsx
+++ src/components/SessionItem.tsx
@@ -15,16 +15,14 @@ export const SessionItem = ({ response }: SessionItemProps): JSX.Element => {
   const formattedDate = `${date.toLocaleString('default', { month: 'long' })} ${date.getDate()}, ${date.getFullYear()}`
 
   return (
-    <React.Fragment key={response.id}>
-      <div>
-        <div className="flex flex-row w-full inline-block text-left my-1 p-4 leading-tight bg-white rounded-md">
-          <Avatar initials={response.student.initials} />
-          <div className="inline-block flex flex-col">
-            <p className="block font-semibold">{name}</p>
-            <p className="block text-xs">{formattedDate}</p>
-          </div>
+    <div>
+      <div className="flex flex-row w-full inline-block text-left my-1 p-4 leading-tight bg-white rounded-md">
+        <Avatar initials={response.student.initials} />
+        <div className="inline-block flex flex-col">
+          <p className="block font-semibold">{name}</p>
+          <p className="block text-xs">{formattedDate}</p>
         </div>
       </div>
-    </React.Fragment>
+    </div>
   )
 }
