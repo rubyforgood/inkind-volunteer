5459d3cd7d5720c06de63bff01a169a7f0ecc4c5
Kelly Ryan <kellyeryan@gmail.com>
2021-10-27 13:44:34 -0400

    Fix date test bug
    Date was hardcoded so when checking age it would get out of sync and the test would fail

--- src/__test__/components/StudentSection.test.tsx
+++ src/__test__/components/StudentSection.test.tsx
@@ -8,7 +8,12 @@ import { Router } from 'react-router-dom'
 import { createMemoryHistory } from 'history'
 
 test("renders student section", async () => {
-  const students: Student[] = [{ name: "John Smith", dateOfBirth: "2008-10-20" }]
+  const date = new Date()
+  const additionOfYears = 13
+  date.setFullYear(date.getFullYear() - additionOfYears)
+  const formattedDate = date.toISOString().slice(0,10)
+
+  const students: Student[] = [{ name: "John Smith", dateOfBirth: formattedDate }]
   const history = createMemoryHistory()
 
   renderWithQueryProvider(
@@ -18,7 +23,7 @@ test("renders student section", async () => {
   )
   await waitFor(() => {
     const studentName = screen.getByText(/John Smith/)
-    const studentDateOfBirth = screen.getByText(/Age 12/)
+    const studentDateOfBirth = screen.getByText(/Age 13/)
 
     expect(studentName).toBeInTheDocument()
     expect(studentDateOfBirth).toBeInTheDocument()
