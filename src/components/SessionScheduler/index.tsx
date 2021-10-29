3f2647fe830d2d8a0d7c66882f832b5939137066
Gillian Wenhold <gillian.wenhold@powerhrg.com>
2021-10-29 10:39:59 -0400

    Trying things for input

--- src/components/SessionScheduler/index.tsx
+++ src/components/SessionScheduler/index.tsx
@@ -13,11 +13,13 @@ interface ScheduleInput {
 }
 
 export const SessionScheduler = (): JSX.Element => {
-  const [selectedDate, setSelectedDate] = useState(null)
-  const [sessionDuration, setSessionDuration] = useState("Select session duration")
+  const [showDurationInput, setShowDurationInput] = useState(false)
+  const [selectedDuration, setSelectedDuration] = useState<string>("Select session duration")
   const { control, register, handleSubmit, formState: { errors } } = useForm<ScheduleInput>()
 
+  const toggleShowDuration = () => setShowDurationInput(!showDurationInput)
   const onSubmit = (data: ScheduleInput) => console.log(data)
+
   return (
     <div className="text-center w-full flex items-center flex-col px-4 py-8 pt-8 text-neutral-600">
       <div className="w-full">
@@ -37,19 +39,63 @@ export const SessionScheduler = (): JSX.Element => {
                       selected={field.value}
                   />
                 )}
-              />
+            />
             <div className="bg-white shadow appearance-none border rounded w-full py-4 px-3 text-neutral-600 leading-tight focus:outline-none focus:shadow-outline">
-              <div className="flex flex-row justify-between">
-                <p>
-                  {sessionDuration}
-                </p>
-                <img
-                    src={String(TimerIcon,)}
-                    width="16px"
-                    height="16px"
-                />
+              <div onClick={toggleShowDuration}>
+                <div className="flex flex-row justify-between">
+                  <p>
+                    Select session duration
+                  </p>
+                  <img
+                      src={String(TimerIcon,)}
+                      width="16px"
+                      height="16px"
+                  />
+                </div>
               </div>
             </div>
+            {showDurationInput && (
+              <>
+                <div className="flex flex-col text-left">
+                  <label htmlFor="field-30-mins">
+                    <input
+                        className="my-4"
+                        id="field-30-mins"
+                        type="radio"
+                        value="30 minutes"
+                    />
+                    30 minutes
+                  </label>
+                  <label htmlFor="field-60-mins">
+                      <input
+                          className="my-4"
+                          id="field-60-mins"
+                          type="radio"
+                          value="60 minutes"
+                      />
+                      60 minutes
+                  </label>
+                  <label htmlFor="field-90-mins">
+                      <input
+                          className="my-4"
+                          id="field-90-mins"
+                          type="radio"
+                          value="90 minutes"
+                      />
+                      90 minutes
+                  </label>
+                  <label htmlFor="field-other">
+                      <input
+                          className="my-4"
+                          id="field-other"
+                          type="radio"
+                          value="other"
+                      />
+                      Other
+                  </label>
+                </div>
+              </>
+            )}
           </div>
         </form>
       </div>
