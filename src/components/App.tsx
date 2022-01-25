581e6d55c905a7d4a8c807047e09526e1b53623f
Sean Marcia <smarcia@gmail.com>
2022-01-24 23:23:21 -0500

    test

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -19,7 +19,7 @@ const queryClient = new QueryClient()
 
 const uriLink = () => {
   // if (window.location.href.indexOf("inkind-volunteer.herokuapp.com") > -1) {
-    return "http://inkind-admin.herokuapp.com/graphql"
+    return "https://cors-anywhere.herokuapp.com/https://inkind-admin.herokuapp.com/graphql"
   // } else {
   //   return "http://localhost:3001/graphql"
   // }
